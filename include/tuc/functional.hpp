265fd411d4e8936b8decbccd6da5f907354a9121
Juha Reunanen <juha.reunanen@tomaattinen.com>
2019-03-24 11:03:50 +0200

    Add explicit reserve() for std::vector outputs - Perhaps std::transform is smart enough that this is not really needed - but let's keep it as an example of some template code

--- include/tuc/functional.hpp
+++ include/tuc/functional.hpp
@@ -1,14 +1,25 @@
 #pragma once
 
-namespace tuc
-{
+#include <vector>
 
+namespace tuc
+{    
     template <typename Output, typename Input, typename MapFunction>
     Output map(Input const& input, MapFunction function)
     {
         Output output;
+        detail::reserve(output, input.size()); // Perhaps std::transform is smart enough that this is not really needed for performance.
         std::transform(input.begin(), input.end(), std::back_inserter(output), function);
         return output;
+    }
+
+    namespace detail {
+        template <typename NotVector> void reserve(NotVector& not_vector, size_t new_capacity) {
+            // By default, do nothing (not std::vector)
+        }
+        template <typename VectorElement> void reserve(std::vector<VectorElement>& vector, size_t new_capacity) {
+            vector.reserve(new_capacity);
+        }
     }
 
 }
