329f9bb8b3967b6c47f7c44a78e1853ed7f308a5
Juha Reunanen <juha.reunanen@tomaattinen.com>
2022-02-19 15:26:55 +0200

    #define WIN32_LEAN_AND_MEAN and NOMINMAX only if not already defined

--- include/tuc/thread.hpp
+++ include/tuc/thread.hpp
@@ -3,10 +3,14 @@
 #include <thread>
 
 #ifdef WIN32
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif // WIN32_LEAN_AND_MEAN
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif // NOMINMAX
 #include <windows.h>
-#endif
+#endif // WIN32
 
 namespace tuc
 {
