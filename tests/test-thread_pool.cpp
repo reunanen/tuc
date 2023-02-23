ac51d9b13d909a997cea70105965b901928cd962
Juha Reunanen <juha.reunanen@tomaattinen.com>
2023-02-23 19:51:11 +0200

    Fix Appveyor test

--- tests/test-thread_pool.cpp
+++ tests/test-thread_pool.cpp
@@ -212,14 +212,14 @@ namespace {
         tuc::thread_pool tp;
         EXPECT_EQ(tp.get_thread_count(), std::thread::hardware_concurrency());
 
-        tp.set_thread_count(1);
-        EXPECT_EQ(tp.get_thread_count(), 1);
+        tp.set_thread_count(1u);
+        EXPECT_EQ(tp.get_thread_count(), 1u);
 
-        tp.set_thread_count(4);
-        EXPECT_EQ(tp.get_thread_count(), 4);
+        tp.set_thread_count(4u);
+        EXPECT_EQ(tp.get_thread_count(), 4u);
 
-        tp.set_thread_count(2);
-        EXPECT_EQ(tp.get_thread_count(), 2);
+        tp.set_thread_count(2u);
+        EXPECT_EQ(tp.get_thread_count(), 2u);
     }
 
 }  // namespace
