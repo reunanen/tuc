080459173da8bb0fbafe832e35d398c7c7207acf
Juha Reunanen <juha.reunanen@tomaattinen.com>
2023-09-08 17:37:07 +0300

    Add new function `tuc::throttle::reset`

--- tests/test-throttle.cpp
+++ tests/test-throttle.cpp
@@ -24,4 +24,20 @@ namespace {
         EXPECT_GT(action_counter, 5);
         EXPECT_LT(action_counter, 50);
     }
+
+    TEST_F(ThrottleTest, ResetsThrottle) {
+        tuc::throttle t;
+        int check_counter = 0;
+        int action_counter = 0;
+        for (int i = 0; i < 100; ++i) {
+            ++check_counter;
+            t.reset();
+            if (t.act_if_appropriate(std::chrono::milliseconds(100))) {
+                ++action_counter;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        EXPECT_EQ(check_counter, 100);
+        EXPECT_EQ(action_counter, 100);
+    }
 }  // namespace
